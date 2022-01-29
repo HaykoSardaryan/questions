0a8abe59f9a102708eff9bfdd4c252617d7ca35b
HaykoSardaryan <sardaryan.03@mail.ru>
2022-01-29 13:12:24 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,124 @@
+// let i = 0
+// while(i < 10) {
+//     i++;
+//     console.log(i);
+// }
+
+// let i = 1,
+//      fact = 1,
+//      nun = 5;
+
+// while(i <= nun) {
+//     fact = fact * i;
+//     console.log(fact);
+//     i++
+// }
+
+// console.log(fact);
+
+// const person = new Object()
+
+// person.firstName = 'Hayk'
+// person.lastName = 'Sardaryan'
+// person.age = 18
+
+// console.log(person);
+
+// const person = {
+//     firstName: 'Hayk',
+//     lastName: 'Sardaryan',
+//     age: 18
+// }
+
+// console.log(person);
+
+// const Language = Spain
+
+// let masssage;
+// if(Language === 'English') {
+//     masssage = 'Hello'
+// }else if (Language === 'Russian') {
+//     masssage = 'privet'
+
+// }else if (Language === 'Spain') {
+//     masssage = 'hola'
+// }
+
+// console.log();
+
+
+
+
+// const LanguaageData1 = {
+//     English: 'Hello',
+//     Spain: 'hola',
+//     x: 10,
+//     y: 20
+// } 
+
+// const LanguaageData2 = {...LanguaageData1}
+
+// LanguaageData2.Rusian = 'privet'
+
+// console.log(LanguaageData1, 'LanguaageData1');
+// console.log(LanguaageData2, 'LanguaageData2');
+
+
+
+// const array = [false, 1, 'Hallo', NaN, null, 7, true, 'JavaScript'];
+
+// const nmb = [];
+// const str = [];
+// const boo = [];
+
+// for(let i = 0; i < array.length; i++) {
+//     if(typeof array[i] === 'string') {
+//          str.push(array[i])
+//     }else if(typeof array[i] === 'boolean') {
+//         boo.push(array[i])
+//     }else if(typeof array[i] === 'number' && !isNaN(array[i])) {
+//         nmb.push(array[i])
+
+//     }
+// }
+ 
+
+// const array = [7, 2, 9, 4, 5, 7, 1, 2, 5, 9, 4,];
+// const arr = []
+// for(let i=0; i < array.length; i++) {
+//     console.log();
+//     if(!arr.includes(array[i])) {
+//         arr.push(array[i])
+//     }
+// }
+
+
+// function calc(opearnd1, opearnd2, operator) {
+//     let result;
+//     switch(operator) {
+//         case '+': result = opearnd1 + opearnd2;break;
+//         case '-': result = opearnd1 - opearnd2;break;
+//         case '/': result = opearnd1 / opearnd2;break;
+//         case '*': result = opearnd1 * opearnd2;break;
+//     }
+//     console.log(`${opearnd1} ${operator} ${opearnd2} = ${result}`);
+// }
+
+// calc(10, 20, '+')
+
+
+
+// const arr = [4, 2, 1, 6, 7, 9, 10, 3, 5, 8];
+
+// function mySort(array) {
+//     let count = 0;
+//     for(let i = 0; i < array.length; i++) {
+//         let minIndex = i;
+//         for(let j =i + 1; j < array.length; j++) {
+
+//         }
+//     }
+
+// }
+
+// mySort(arr)
